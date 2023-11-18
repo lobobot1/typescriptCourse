aa4c2ae7cd12746b568fe21a4dfc98b34aac6644
kevin <kevincolmenaresm@gmail.com>
2023-11-17 22:05:15 -0400

    Add tuple example with type restrictions

---
+++ src/02-turplas.ts
@@ -0,0 +1,15 @@
+const prices: number[] = [1,2,3];
+
+//I only add in a turple the same type of data and elements
+const names: [string,number] = ['nico',1];
+
+//I can't add more than 2 elements
+// const names: [string,number] = ['nico',1,2];
+
+//I can't add different types of data
+// const names: [string,number] = ['nico',1,true];
+
+//Ican't add less than 2 elements
+// const names: [string,number] = ['nico'];
+
+console.log(names)
